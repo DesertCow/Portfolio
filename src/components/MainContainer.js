589c53827e5ce53d401f4cc306ad982e7e202a2d
Clayton S <desertcow07@gmail.com>
2022-07-19 19:46:52 -0700

    Got HTML to render!

---
+++ src/components/MainContainer.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import NavTabs from './Navigation';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+
+
+
+  return (
+
+    // <div>
+    //   {/* // TODO: Add a comment describing what we are passing as props */}
+    //   <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+    //   {/* // TODO: Add a comment explaining what is happening on the following line */}
+    //   {renderPage()}
+    // </div>
+
+    <div>
+      <h1> Test TEst TEst!</h1>
+      <h2> Test TEst TEst!</h2>
+      <h3> Test TEst TEst!</h3>
+    </div>
+
+  );
+
+}
