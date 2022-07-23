c35d318eaabd4803253cfb238cca281e16b5087d
Clayton S <desertcow07@gmail.com>
2022-07-22 17:38:50 -0700

    Added GITHUB links for each app

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -45,21 +45,36 @@ export default function Home() {
           <div className="d-flex m-2 justify-content-evenly">
             <div className="d-flex row AppBox mt-5">
               <h2 className="flex-row text-center mt-3 p-3">Dinner With Schmucks</h2>
-              <a href="https://desertcow.github.io/DinnerWithSchmucks" className="d-flex flex-row justify-content-center align-items-center mb-5 mt-3 p-5">
+              <a className="text-center" href="https://github.com/DesertCow/DinnerWithSchmucks">
+                <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="grey" className="bi bi-github footerIcon" viewBox="0 0 16 16">
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+              </a>
+              <a href="https://desertcow.github.io/DinnerWithSchmucks" className="d-flex flex-row justify-content-center align-items-center mb-5 mt-3 p-3">
                 <img className="AppGIF AppGIF1 img-fluid" src="https://github.com/DesertCow/DinnerWithSchmucks/blob/main/assets/images/DinnerWithSchmucks_Demo.gif?raw=true" alt="Dinner For Schmucks Demo GIF"></img>
               </a>
             </div>
-            <div className="AppBox mt-5 flex-row mt-3 p-3 justify-content-evenly">
-              <h2 className="flex-row text-center">Wx Dashboard </h2>
-              <a href="https://desertcow.github.io/WxDashboard" className="d-flex justify-content-center align-items-center mb-5 mt-3 p-5">
+            <div className="d-flex row AppBox mt-5">
+              <h2 className="flex-row text-center mt-3">Wx Dashboard </h2>
+              <a className="text-center mt-3 mb-0" href="https://github.com/DesertCow/WxDashboard">
+                <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="grey" className="bi bi-github footerIcon" viewBox="0 0 16 16">
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+              </a>
+              <a href="https://desertcow.github.io/WxDashboard" className="d-flex justify-content-center align-items-center mb-5 mt-3 p-3">
                 <img className="AppGIF AppGIF2 img-fluid" src="https://github.com/DesertCow/WxDashboard/raw/main/assets/devMeta/Final_Demo.gif?raw=true" alt="WX Dashboard Demo GIF"></img>
               </a>
             </div>
           </div>
           <div className="d-flex m-2 justify-content-evenly">
             <div className="d-flex row AppBox mt-5">
-              <h2 className="flex-row text-center">Surf-Deck </h2>
-              <a href="https://surf-deck.herokuapp.com/" className="d-flex justify-content-center align-items-center mb-5 mt-3 p-5">
+              <h2 className="flex-row text-center mt-3">Surf-Deck </h2>
+              <a className="text-center mt-3" href="https://github.com/DesertCow/Surf-Deck">
+                <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="grey" className="bi bi-github footerIcon" viewBox="0 0 16 16">
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+              </a>
+              <a href="https://surf-deck.herokuapp.com/" className="d-flex justify-content-center align-items-center mb-5 mt-3 p-3">
                 <img className="AppGIF AppGIF3 img-fluid" src="https://github.com/DesertCow/Surf-Deck/raw/main/public/img/demo1.gif" alt="Surf Deck Demo"></img>
               </a>
             </div>
